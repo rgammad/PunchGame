63d32447660e0ac8abfb15edad001de25744da3b
Roldan Gammad <rgammad@uci.edu>
2017-11-04 17:28:43 -0700

    saved test scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,38 @@
 ﻿
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+
     public float moveSpeed = 5;
+    public float jumpPower = 5;
     private Vector3 movement;
+    private bool isJumping = false;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        isJumping = false;
+        rigid = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Jump"))
+            isJumping = true;
         movement = new Vector3(horizontal, 0, vertical);
     }
+
+
     private void FixedUpdate()
     {
         transform.Translate(movement * Time.deltaTime * moveSpeed);
+        if (isJumping)
+        {
+            rigid.AddForce(new Vector3(0, jumpPower, 0));
+            isJumping = false;
+        }
     }
 }
