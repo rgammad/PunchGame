ad9434c799bdf3f9097f94fbe1fd9e3ded5be4e4
Roldan Gammad <rgammad@uci.edu>
2017-11-04 17:11:46 -0700

    Added basic player controller and third person camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    public float rotateSpeed = 5;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = target.transform.position - transform.position;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+        transform.LookAt(target.transform);
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed = 5;
+    private Vector3 movement;
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement = new Vector3(horizontal, 0, vertical);
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(movement * Time.deltaTime * moveSpeed);
+    }
+}
